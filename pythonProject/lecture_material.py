e07769237a8717cb6fa20bc2411cc8ae098145a1
Dot Net By Vickypedia <vickyraj.501@gmail.com>
2024-10-04 19:38:13 +0530

    Initial Commit

---
+++ pythonProject/lecture_material.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+integer = 7
+float_num = 2.718
+bool_val = False
+#integer = 10
+
+add_assign = 5
+add_assign = add_assign + 7
+add_assign += 7
+print(add_assign)
+
+sub = 10
+sub -= 5
+print(sub)
+
+mult = 10
+mult *= 5
+print(mult)
+
+div = 10
+div /=5
+print(div)
+
+
